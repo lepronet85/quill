699ebd022305197250b9d0088afa484ce25de27a
lepronet85 <lepronet85@gmail.com>
2023-12-21 19:55:01 +0100

    Update files

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Image from "next/image";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <MaxWidthWrapper className="Mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center">
+      <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50">
+        <p className="text-sm font-semibold text-gray-700">
+          Quill is now public!
+        </p>
+      </div>
+    </MaxWidthWrapper>
+  );
 }
--- components/MaxWidthWrapper.tsx
+++ components/MaxWidthWrapper.tsx
@@ -1,14 +1,20 @@
+import { cn } from "@/lib/utils";
 import { ReactNode } from "react";
 
 const MaxWidthWrapper = ({
   className,
   children,
 }: {
-  className: string;
+  className?: string;
   children: ReactNode;
 }) => {
   return (
-    <div className="mx-auto w-full max-w-screen-xl px-2.5 md:px-20">
+    <div
+      className={cn(
+        "mx-auto w-full max-w-screen-xl px-2.5 md:px-20",
+        className
+      )}
+    >
       {children}
     </div>
   );

c2b0f1b809756753f75e65ee5d7579ac7b226782
lepronet85 <lepronet85@gmail.com>
2024-01-01 17:22:30 +0100

    Update

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,13 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
 
 const Page = async () => {
   const { getUser } = getKindeServerSession();
   const user = await getUser();
 
-  return <div>{user?.email}</div>;
+  if (!user || !user.id) redirect("/auth-callback?origin=dashboard");
+
+  return <div>{user?.picture}</div>;
 };
 
 export default Page;

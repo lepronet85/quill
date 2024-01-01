f57a9d4ec501a02794533c5503b3fc5d9e1cb8a6
lepronet85 <lepronet85@gmail.com>
2024-01-01 18:18:37 +0100

    Update

--- app/auth-callback/page.tsx
+++ app/auth-callback/page.tsx
@@ -1,9 +1,18 @@
 import { useRouter, useSearchParams } from "next/navigation";
+import { trpc } from "../_trpc/client";
 
 const Page = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
   const origin = searchParams.get("origin");
+
+  const { data, isLoading } = trpc.authCallback.useQuery(undefined, {
+    onSuccess: (success) => {
+      if (success) {
+        router.push();
+      }
+    },
+  });
 };
 
 export default Page;
---
+++ trpc/index.ts
@@ -0,0 +1,16 @@
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/dist/types/server";
+import { publicProcedure, router } from "./trpc";
+import { TRPCError } from "@trpc/server";
+
+export const appRouter = router({
+  authCallback: publicProcedure.query(async () => {
+    const { getUser } = getKindeServerSession();
+    const user = await getUser();
+
+    if (!user?.email || !user.id) throw new TRPCError({ code: "UNAUTHORIZED" });
+
+    return { success: true };
+  }),
+});
+
+export type AppRouter = typeof appRouter;
